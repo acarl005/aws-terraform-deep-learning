d3fea0098ed1db83f78f27eb784a26ca738227cf
Zen Tang <zen@wayve.ai>
2020-05-31 13:35:14 -0500

    run terraform 0.12upgrade

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # you'll need to provide credentials in ~/.aws/credentials
 # make sure you add the key and secret under the [terraform] profile
-provider aws {
+provider "aws" {
   region  = "us-east-1"
   profile = "terraform"
 }
@@ -10,16 +10,16 @@ resource "aws_vpc" "tf" {
   instance_tenancy     = "dedicated"
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
 
 # allow our VPC to talk to the public internet
 resource "aws_internet_gateway" "tf" {
-  vpc_id = "${aws_vpc.tf.id}"
+  vpc_id = aws_vpc.tf.id
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
@@ -31,41 +31,41 @@ locals {
 
 # define the subnet to put our instance in
 resource "aws_subnet" "tf" {
-  vpc_id                  = "${aws_vpc.tf.id}"
-  cidr_block              = "${aws_vpc.tf.cidr_block}"
+  vpc_id                  = aws_vpc.tf.id
+  cidr_block              = aws_vpc.tf.cidr_block
   map_public_ip_on_launch = true
-  availability_zone       = "${local.avail_zone}"
+  availability_zone       = local.avail_zone
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
 
 # this is for the default route table that was created with our VPC
 resource "aws_default_route_table" "rt" {
-  default_route_table_id = "${aws_vpc.tf.default_route_table_id}"
+  default_route_table_id = aws_vpc.tf.default_route_table_id
 
   # make sure all outbound traffic goes through the internet gateway
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.tf.id}"
+    gateway_id = aws_internet_gateway.tf.id
   }
 
-  tags {
+  tags = {
     Name = "default table"
   }
 }
 
 # attach route table to the sbnet
 resource "aws_route_table_association" "rt_assoc" {
-  subnet_id      = "${aws_subnet.tf.id}"
-  route_table_id = "${aws_vpc.tf.default_route_table_id}"
+  subnet_id      = aws_subnet.tf.id
+  route_table_id = aws_vpc.tf.default_route_table_id
 }
 
 # permit inbound access to all the ports we need
 resource "aws_security_group" "tf" {
   name   = "allow_8888"
-  vpc_id = "${aws_vpc.tf.id}"
+  vpc_id = aws_vpc.tf.id
 
   # jupyter uses 8888 by default
   ingress {
@@ -99,7 +99,7 @@ resource "aws_security_group" "tf" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
@@ -107,15 +107,15 @@ resource "aws_security_group" "tf" {
 # to permit SSH access
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
-  public_key = "${file("${var.public_key_file}")}"
+  public_key = file(var.public_key_file)
 }
 
 # a volume to persist our model after training
 resource "aws_ebs_volume" "tf" {
-  availability_zone = "${local.avail_zone}"
-  size              = "${var.volume_size_gb}"
+  availability_zone = local.avail_zone
+  size              = var.volume_size_gb
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
@@ -126,29 +126,30 @@ resource "aws_instance" "ec2" {
   ami = "ami-7336d50e"
 
   # a type with a beefy GPU is required
-  instance_type          = "${var.instance_type}"
-  availability_zone      = "${local.avail_zone}"
-  subnet_id              = "${aws_subnet.tf.id}"
-  vpc_security_group_ids = ["${aws_security_group.tf.id}"]
+  instance_type          = var.instance_type
+  availability_zone      = local.avail_zone
+  subnet_id              = aws_subnet.tf.id
+  vpc_security_group_ids = [aws_security_group.tf.id]
   key_name               = "ssh_key"
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.tf.id}"
-  instance_id = "${aws_instance.ec2.id}"
+  volume_id   = aws_ebs_volume.tf.id
+  instance_id = aws_instance.ec2.id
 }
 
 # assign a constant IP that we can reach
 resource "aws_eip" "tf" {
-  instance = "${aws_instance.ec2.id}"
+  instance = aws_instance.ec2.id
   vpc      = true
 
-  tags {
+  tags = {
     Name = "tf"
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "instance_id" {
-  value = "${aws_instance.ec2.id}"
+  value = aws_instance.ec2.id
 }
 
 output "instance_user_name" {
@@ -7,17 +7,18 @@ output "instance_user_name" {
 }
 
 output "instance_elastic_ip" {
-  value = "${aws_eip.tf.public_ip}"
+  value = aws_eip.tf.public_ip
 }
 
 output "instance_public_ip" {
-  value = "${aws_instance.ec2.public_ip}"
+  value = aws_instance.ec2.public_ip
 }
 
 output "instance_public_dns" {
-  value = "${aws_instance.ec2.public_dns}"
+  value = aws_instance.ec2.public_dns
 }
 
 output "instance_private_ip" {
-  value = "${aws_instance.ec2.private_ip}"
+  value = aws_instance.ec2.private_ip
 }
+
