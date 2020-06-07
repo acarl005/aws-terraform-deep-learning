ab184f9d05309b5a13dc63ce231cdf609fd91ac6
Zen Tang <zen@wayve.ai>
2020-06-07 10:29:54 -0500

    eliminate $2/hour surcharge on instances

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_vpc" "tf" {
   cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "dedicated"
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
 
   tags = {
